2e149488088368b2c8005c827c3a053724d8b677
SC1314520 <s112029013@smail.nchu.edu.tw>
2023-12-14 00:04:48 +0800

    s

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ const vm =new Vue({
             .catch(err => console.error(err));
         },
         getPopularMovie(index,flag){
-            this.scrollToTop(0);
+            this.scrollToTop();
             this.toggleBar(flag);
             this.pageType=1;
             this.searchFlag.searchName='';
