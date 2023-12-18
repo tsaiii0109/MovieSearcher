e7b0804ed90223ec2437d1ec973249958e3b95fa
SC1314520 <s112029013@smail.nchu.edu.tw>
2023-12-18 11:30:45 +0800

    s

--- gs/query.js
+++ gs/query.js
@@ -1,4 +1,5 @@
-const sheet =SpreadsheetApp.getActive();
+const sheet =SpreadsheetApp.getActive().getSheetByName('main');
+const listSheet =SpreadsheetApp.getActive().getSheetByName('list');
 function doGet(){
   var data = sheet.getRange('A1').getValue();
   return ContentService.createTextOutput(data).setMimeType(ContentService.MimeType.JSON);
@@ -12,7 +13,17 @@ function query(){
   }
   const results =YouTube.Search.list('id,snippet',option);
   sheet.getRange('A1').setValue(JSON.stringify(results));
+  setList(results);
 }
 function getTime(){
   return new Date(new Date().getTime()-1000*86400*21).toISOString();
+}
+function setList(results){
+  listSheet.getDataRange().clear();
+  var str = sheet.getRange('A1').getValue();
+  var obj = results.items;
+  for(var i=0;i<obj.length;i++){
+    var snippet =obj[i]['snippet']
+    listSheet.appendRow([snippet['title'],snippet['description'],snippet['publishTime']])
+  }
 }
